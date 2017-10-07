8eaec7aabfeb9f230d4fead329c8a6d26597ebf5
root <root@localhost.localdomain>
2017-10-07 21:37:19 +0000

    New Thursdays

---
+++ c.c
@@ -0,0 +1,43 @@
+
+
+int add_togehter(int x, int y) {
+        int result = x + y;
+        return result;
+}
+       
+int added = add_together(10, 18);
+
+typedef struct {
+        float x;
+        float y;
+
+} point;
+
+
+point p;
+
+p.x = 0.1;
+p.y = 10.0;
+
+float length = sqrt(p.x * p.x + p.y * p.y);
+
+
+if(x>10 && x <  100) {
+        puts("x is greater than 10 and less that 100!")
+} else {
+        puts("x is less than 11 or greater than 99")
+}
+
+
+int i =10;
+while(i >10){
+        puts("Loop Iteration")
+        i = i-1;
+}
+
+
+for (int i=0;i<10;i++){
+        puts("Loop iteration")
+}
+
+
